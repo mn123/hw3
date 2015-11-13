1b85b98c1e96361fe8cfd3bc970b8ac8ef877580
mn123 <matthias.neufeld@hhu.de>
2015-11-13 18:17:15 +0100

    homework3 done

--- statistics.cxx
+++ statistics.cxx
@@ -3,17 +3,26 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void f(double* x, int N, double& mean, double& var){
+	for(int i=0; i<N; i++){
+		x[i]  = rand()/double(RAND_MAX);
+		mean += x[i];
+	}
+	mean = mean/N;
+	for(int i=0; i<N; i++)
+		var += (x[i]-mean)*(x[i]-mean);
+	var = var/N;
+}
 
 int main(){
+	srand(time(NULL));
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
 
+	f(p,N,mean,var);
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
